5c912badeab211b307c7158767da4a96efb94a29
lixin <283202734@qq.com>
2020-06-18 23:07:50 +0800

    使用线程交替打印1234567和ABCDEFG

---
+++ src/main/java/com/juc/LockConditionInTread.java
@@ -0,0 +1,53 @@
+package com.juc;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 使用线程交替打印1234567和ABCDEFG
+ * 使用ReentrantLock和Condition实现
+ */
+public class LockConditionInTread {
+    public static void main(String[] args) {
+        char[] aI = "1234567".toCharArray();
+        char[] aC = "ABCDEFG".toCharArray();
+
+        Lock lock = new ReentrantLock();
+        Condition condition1 = lock.newCondition();
+        Condition condition2 = lock.newCondition();
+
+        new Thread(() -> {
+            try {
+                lock.lock();
+                for (char c : aC) {
+                    System.out.print(c);
+                    condition2.signal();
+                    condition1.await();
+                }
+                condition2.signal();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }, "t1").start();
+
+        new Thread(() -> {
+            try {
+                lock.lock();
+                for (char c : aI) {
+                    System.out.print(c);
+                    condition1.signal();
+                    condition2.await();
+                }
+                condition1.signal();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }, "t2").start();
+
+    }
+}
---
+++ src/main/java/com/juc/TransferQueueInTread.java
@@ -0,0 +1,39 @@
+package com.juc;
+
+import java.util.concurrent.LinkedTransferQueue;
+import java.util.concurrent.TransferQueue;
+
+/**
+ * 使用线程交替打印1234567和ABCDEFG
+ * 使用TransferQueue实现
+ */
+public class TransferQueueInTread {
+    public static void main(String[] args) {
+        char[] aI = "1234567".toCharArray();
+        char[] aC = "ABCDEFG".toCharArray();
+        TransferQueue<Character> transferQueen = new LinkedTransferQueue<>();
+
+        new Thread(() -> {
+            try {
+                for (char c : aI) {
+                    System.out.print(transferQueen.take());
+                    transferQueen.transfer(c);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "t1").start();
+
+        new Thread(() -> {
+            try {
+                for (char c : aC) {
+                    transferQueen.transfer(c);
+                    System.out.print(transferQueen.take());
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "t2").start();
+
+    }
+}

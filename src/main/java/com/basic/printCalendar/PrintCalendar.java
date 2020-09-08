81c0286677ef5008b4b63e0f5ebe665be83385db
lixin <283202734@qq.com>
2020-09-08 08:14:53 +0800

    打印当月日历

--- src/main/java/com/basic/printCalendar/PrintCalendar.java
+++ src/main/java/com/basic/printCalendar/PrintCalendar.java
@@ -5,12 +5,12 @@ import java.time.LocalDate;
 
 /**
  * 打印当月日历
- * Mon Tue Wed Thu Fri Sat Sun
- *       1   2   3   4   5   6
- *   7*  8   9  10  11  12  13
- *  14  15  16  17  18  19  20
- *  21  22  23  24  25  26  27
- *  28  29  30
+ * Sun Mon Tue Wed Thu Fri Sat
+ *           1   2   3   4   5
+ *   6   7   8*  9  10  11  12
+ *  13  14  15  16  17  18  19
+ *  20  21  22  23  24  25  26
+ *  27  28  29  30
  */
 public class PrintCalendar {
     public static void main(String[] args) {
@@ -22,8 +22,8 @@ public class PrintCalendar {
         DayOfWeek dayOfWeek = date.getDayOfWeek();
         int value = dayOfWeek.getValue();
 
-        System.out.println("Mon Tue Wed Thu Fri Sat Sun");
-        for (int i = 1; i < value; i++) {
+        System.out.println("Sun Mon Tue Wed Thu Fri Sat");
+        for (int i = 0; i < value; i++) {
             System.out.print("    ");
         }
 
@@ -36,12 +36,12 @@ public class PrintCalendar {
             }
 
             date = date.plusDays(1);
-            if (date.getDayOfWeek().getValue() == 1) {
+            if (date.getDayOfWeek().getValue() == 7) {
                 System.out.println();
             }
         }
 
-        if (date.getDayOfWeek().getValue() != 1) {
+        if (date.getDayOfWeek().getValue() != 7) {
             System.out.println();
         }
 

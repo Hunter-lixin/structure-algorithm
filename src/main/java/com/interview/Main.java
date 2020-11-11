0d8d902d05e217e8727cedbb0a67e8868d6694bc
lixin <283202734@qq.com>
2020-11-11 08:55:26 +0800

    整理

--- src/main/java/com/interview/Main.java
+++ src/main/java/com/interview/Main.java
@@ -1,10 +1,5 @@
 package com.interview;
 
-import org.junit.jupiter.api.Test;
-import sun.jvm.hotspot.utilities.Interval;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 /**
  * 给定一个非负整数数组，a1,a2,...,an,和一个目标数s。现在你有两个符号+和-。
  * 对于数组中任意一个整数，你都可以从+或- 中选择一个符号添加在前面。
@@ -55,14 +50,5 @@ public class Main {
         System.out.println(sumCount(nums, sum));
 
     }
-
-    /*@Test
-    public void testNumsSum() {
-        main
-        int[] nums = new int[]{1, 1, 1, 1, 1};
-        int sum = 3;
-        assertEquals();
-    }
-*/
 
 }

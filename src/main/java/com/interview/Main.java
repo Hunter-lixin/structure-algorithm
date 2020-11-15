37febbe9f93c1e17247737b1e11a1ec8c14a7dea
lixin <283202734@qq.com>
2020-11-16 06:49:15 +0800

    求穿过砖头数

--- src/main/java/com/interview/Main.java
+++ src/main/java/com/interview/Main.java
@@ -1,54 +1,76 @@
 package com.interview;
 
 /**
- * 给定一个非负整数数组，a1,a2,...,an,和一个目标数s。现在你有两个符号+和-。
- * 对于数组中任意一个整数，你都可以从+或- 中选择一个符号添加在前面。
+ * 示例：
+ * 输入: [[1,2,2,1],
+ * [3,1,2],
+ * [1,3,2],
+ * [2,4],
+ * [3,1,2],
+ * [1,3,1,1]]
  * <p>
- * 返回可以使最终数组和为目标数s的所有添加符号的方法数。
- * <p>
- * 输入： nums:[1, 1, 1, 1, 1], S:3
- * 输出：5
- * <p>
- * 解释：
- * -1+1+1+1+1=3
- * +1-1+1+1+1=3
- * +1+1-1+1+1=3
- * +1+1+1-1+1=3
- * +1+1+1+1-1=3
- * 一共有5种方法最终目标和为3.
+ * 输出: 2
  */
 public class Main {
 
-    public static int sumCount(int[] nums, int sum) {
-        int length = nums.length;
+    public static int getCount(int[][] numsArray) {
         int ans = 0;
-        int numsSum = 0;
-        for (int num : nums) {
-            numsSum += num;
-        }
-        for (int num : nums) {
-            if ((numsSum - num) == (sum + num)) {
-                ans++;
-            }
-        }
 
-        for (int i = 0; i < length; i++) {
-            for (int j = i + 1; j < length; j++) {
-                if ((numsSum - nums[i] - nums[j]) == (sum + nums[i] + nums[j])) {
-                    ans++;
+        int length = numsArray.length;
+        int index = 0;
+        while (index < length) {
+            int[] currNums = numsArray[index];
+            int numsLength = currNums.length;
+
+            int currAns = 0;
+
+            for (int i = 1; i < numsLength - 1; i++) {
+                int currAllValue = getALl(currNums, i);
+
+                int tempAns = 0;
+
+                for (int[] nums : numsArray) {
+                    int temp = 0;
+                    for (int num : nums) {
+                        temp += num;
+                        if (temp == currAllValue) {
+                            break;
+                        }
+                        if (temp > currAllValue) {
+                            tempAns++;
+                            break;
+                        }
+                    }
                 }
+
+                if (currAns == 0) {
+                    currAns = tempAns;
+                } else if (tempAns > 0 && currAns > 0) {
+                    currAns = Math.min(tempAns, currAns);
+                }
+            }
+            index++;
+
+            if (ans == 0) {
+                ans = currAns;
+            } else if (ans > 0 && currAns > 0) {
+                ans = Math.min(ans, currAns);
             }
         }
 
         return ans;
     }
 
-    public static void main(String[] args) {
-        //  全是1 满足
-        int[] nums = new int[]{1, 1, 1, 1, 1};
-        int sum = 3;
-        System.out.println(sumCount(nums, sum));
-
+    public static int getALl(int[] nums, int target) {
+        int ans = 0;
+        for (int i = 0; i < target; i++) {
+            ans += nums[i];
+        }
+        return ans;
     }
 
+    public static void main(String[] args) {
+        int[][] numsArray = new int[][]{{1, 2, 2, 1}, {3, 1, 2}, {1, 3, 2}, {2, 4}, {3, 1, 2}, {1, 3, 1, 1}};
+        System.out.println(getCount(numsArray));
+    }
 }

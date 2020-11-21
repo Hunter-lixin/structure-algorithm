3f1dcee7fc9b0c68490e1cb7faf524d6604a6873
lixin <283202734@qq.com>
2020-11-22 06:49:56 +0800

    QuickSort

---
+++ src/main/java/com/sort/QuickSort.java
@@ -0,0 +1,61 @@
+package com.sort;
+
+/**
+ * 快速排序
+ */
+public class QuickSort {
+
+    public void sort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+
+        process(arr, 0, arr.length - 1);
+    }
+
+    public void process(int[] arr, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+
+        swap(arr, left + (int) (Math.random() * (right - left + 1)), right);
+        int[] equalArea = netherlandsFlag(arr, left, right);
+        process(arr, left, equalArea[0] - 1);
+        process(arr, equalArea[1] + 1, right);
+    }
+
+    public static int[] netherlandsFlag(int[] arr, int left, int right) {
+        if (left > right) {
+            return new int[]{-1, -1};
+        }
+
+        int less = left - 1;
+        int more = right;
+        int index = left;
+
+        while (index < more) {
+            if (arr[index] < arr[right]) {
+                swap(arr, index++, ++less);
+            } else if (arr[index] > arr[right]) {
+                swap(arr, index, --more);
+            } else {
+                index++;
+            }
+        }
+        swap(arr, more, right);
+        return new int[]{less + 1, more};
+    }
+
+    /**
+     * 数组两数交换
+     * @param arr 数组
+     * @param current 当前位置
+     * @param target  目标位置
+     */
+    public static void swap(int[] arr, int current, int target) {
+        if (arr[current] == arr[target]) return;
+        arr[current] = arr[current] ^ arr[target];
+        arr[target] = arr[current] ^ arr[target];
+        arr[current] = arr[current] ^ arr[target];
+    }
+}
---
+++ src/test/java/com/sort/QuickSortTest.java
@@ -0,0 +1,56 @@
+package com.sort;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuickSortTest {
+
+    QuickSort quickSort = new QuickSort();
+
+    @Test
+    void sort_with_one_element() {
+        int[] arr = new int[]{1};
+        int[] expectResult = new int[]{1};
+        quickSort.sort(arr);
+        assertArrayEquals(expectResult, arr);
+    }
+
+    @Test
+    void sort_with_two_element() {
+        int[] arr = new int[]{2, 1};
+        int[] expectResult = new int[]{1, 2};
+        quickSort.sort(arr);
+        assertArrayEquals(expectResult, arr);
+    }
+
+    @Test
+    void sort_with_three_element() {
+        int[] arr = new int[]{3, 2, 1};
+        int[] expectResult = new int[]{1, 2, 3};
+        quickSort.sort(arr);
+        assertArrayEquals(expectResult, arr);
+    }
+
+    @Test
+    void sort_with_random_element() {
+        int[] arr = generateRandomArray(100, 15);
+        int[] expectResult = new int[arr.length];
+        System.arraycopy(arr, 0, expectResult, 0, expectResult.length);
+        quickSort.sort(arr);
+        Arrays.sort(expectResult);
+        assertArrayEquals(expectResult, arr);
+    }
+
+
+    public int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) (Math.random() * (maxSize - 1))];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue - 1) * Math.random());
+        }
+
+        return arr;
+    }
+}

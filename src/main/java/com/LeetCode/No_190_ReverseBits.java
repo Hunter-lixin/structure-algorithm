31d82baf72ba948a80cacc212448a2a718f2addb
lixin <283202734@qq.com>
2021-03-30 06:42:13 +0800

    commit No.190

---
+++ src/main/java/com/LeetCode/No_190_ReverseBits.java
@@ -0,0 +1,12 @@
+package com.LeetCode;
+
+public class No_190_ReverseBits {
+    public int reverseBits(int n) {
+        int rev = 0;
+        for (int i = 0; i < 32 && n != 0; ++i) {
+            rev |= (n & 1) << (31 - i);
+            n >>>= 1;
+        }
+        return rev;
+    }
+}

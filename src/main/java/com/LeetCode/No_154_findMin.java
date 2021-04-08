60394740847ba3bc024d4a92433233917d2d62e3
lixin <283202734@qq.com>
2021-04-09 06:20:27 +0800

    commit No.154 with binary search

---
+++ src/main/java/com/LeetCode/No_154_findMin.java
@@ -0,0 +1,19 @@
+package com.LeetCode;
+
+public class No_154_findMin {
+    public int findMin(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > nums[end]) {
+                start = mid + 1;
+            } else if (nums[mid] < nums[end]) {
+                end = mid;
+            } else {
+                end -= 1;
+            }
+        }
+        return nums[start];
+    }
+}

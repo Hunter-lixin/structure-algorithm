644aa6e67aa9e531694afb92b04e0ca1fd4078c8
lixin <283202734@qq.com>
2021-04-15 06:08:40 +0800

    commit No.783

---
+++ src/main/java/com/LeetCode/No_783_minDiffInBST.java
@@ -0,0 +1,43 @@
+package com.LeetCode;
+
+public class No_783_minDiffInBST {
+    private int ans;
+    private int pre;
+    public int minDiffInBST(TreeNode root) {
+        ans = Integer.MAX_VALUE;
+        pre = -1;
+        dfs(root);
+        return ans;
+    }
+
+    private void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.left);
+        if (pre != -1) {
+            ans = Math.min(ans, root.val - pre);
+        }
+        pre = root.val;
+        dfs(root.right);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}

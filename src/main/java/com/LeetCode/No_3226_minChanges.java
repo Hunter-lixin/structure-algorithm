673d02f43b6f419e0d5be49932fe40984bc940d9
lixin <283202734@qq.com>
2024-11-02 09:57:44 +0800

    commit No_3226_minChanges

---
+++ src/main/java/com/LeetCode/No_3226_minChanges.java
@@ -0,0 +1,18 @@
+package com.LeetCode;
+
+public class No_3226_minChanges {
+    public int minChanges(int n, int k) {
+        int res = 0;
+        while (n > 0 || k > 0) {
+            if ((n & 1) == 0 && (k & 1) == 1) {
+                return -1;
+            }
+            if ((n & 1) == 1 && (k & 1) == 0) {
+                res++;
+            }
+            n >>= 1;
+            k >>= 1;
+        }
+        return res;
+    }
+}

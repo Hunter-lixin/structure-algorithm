7f9ca96fab72afcb165eef0fdde1e4361a8920c2
lixin <283202734@qq.com>
2024-11-17 20:29:13 +0800

    commit No_825_numFriendRequests

---
+++ src/main/java/com/LeetCode/No_825_numFriendRequests.java
@@ -0,0 +1,24 @@
+package com.LeetCode;
+
+import java.util.Arrays;
+
+public class No_825_numFriendRequests {
+    public int numFriendRequests(int[] ages) {
+        int n = ages.length;
+        Arrays.sort(ages);
+        int left = 0, right = 0, ans = 0;
+        for (int age : ages) {
+            if (age < 15) {
+                continue;
+            }
+            while (ages[left] <= 0.5 * age + 7) {
+                ++left;
+            }
+            while (right + 1 < n && ages[right + 1] <= age) {
+                ++right;
+            }
+            ans += right - left;
+        }
+        return ans;
+    }
+}

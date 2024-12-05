648e4f1858b3c644049fb6877e7e95d866d07d69
lixin <283202734@qq.com>
2024-12-06 06:56:11 +0800

    commit No_999_numRookCaptures

---
+++ src/main/java/com/LeetCode/No_999_numRookCaptures.java
@@ -0,0 +1,33 @@
+package com.LeetCode;
+
+public class No_999_numRookCaptures {
+    public int numRookCaptures(char[][] board) {
+        int cnt = 0, st = 0, ed = 0;
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
+        for (int i = 0; i < 8; ++i) {
+            for (int j = 0; j < 8; ++j) {
+                if (board[i][j] == 'R') {
+                    st = i;
+                    ed = j;
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < 4; ++i) {
+            for (int step = 0;; ++step) {
+                int tx = st + step * dx[i];
+                int ty = ed + step * dy[i];
+                if (tx < 0 || tx >= 8 || ty < 0 || ty >= 8 || board[tx][ty] == 'B') {
+                    break;
+                }
+                if (board[tx][ty] == 'p') {
+                    cnt++;
+                    break;
+                }
+            }
+        }
+        return cnt;
+    }
+}
+

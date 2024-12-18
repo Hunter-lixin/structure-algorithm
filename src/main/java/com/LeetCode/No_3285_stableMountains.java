0e77f45487284fe3ac428cd244a0bea3bd1ea840
lixin <283202734@qq.com>
2024-12-19 07:59:34 +0800

    commit No_3285_stableMountains

---
+++ src/main/java/com/LeetCode/No_3285_stableMountains.java
@@ -0,0 +1,16 @@
+package com.LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class No_3285_stableMountains {
+    public List<Integer> stableMountains(int[] height, int threshold) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 1; i < height.length; i++) {
+            if (height[i - 1] > threshold) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
